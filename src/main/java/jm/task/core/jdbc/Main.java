27a4b78a55f6ded40acfe9a832161e2b0df9e9a2
GordinVit <gordin.vit@gmail.com>
2024-07-30 23:16:44 +0300

    codestyle

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,13 +1,7 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.dao.UserDao;
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.util.Util;
 import org.hibernate.SessionFactory;
-
-import java.sql.Connection;
-import java.sql.Driver;
-import java.sql.DriverManager;
 
 public class Main {
 
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -6,65 +6,57 @@ import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Environment;
-
 import java.util.Properties;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
-    private static final String user = "root";
-    private static final String password = "root";
-    private static final String url = "jdbc:mysql://localhost:3306/sakila";
-    private static SessionFactory sessionFactory;
 
+  private static final String USER = "root";
+  private static final String PASSWORD = "root";
+  private static final String URL = "jdbc:mysql://localhost:3306/sakila";
+  private static SessionFactory sessionFactory;
 
-    public static Connection getConnection() {
-        Connection connection = null;
-        try {
-            connection = DriverManager.getConnection(url, user, password);
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return connection;
+  // Method to get a JDBC connection
+  public static Connection getConnection() {
+    Connection connection = null;
+    try {
+      connection = DriverManager.getConnection(URL, USER, PASSWORD);
+    } catch (SQLException e) {
+      throw new RuntimeException("Error connecting to the database", e);
     }
+    return connection;
+  }
 
+  // Method to get a Hibernate SessionFactory
+  public static SessionFactory getSessionFactory() {
+    if (sessionFactory == null) {
+      try {
+        Configuration configuration = new Configuration();
+        Properties settings = new Properties();
+        settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+        settings.put(Environment.URL, URL + "?useSSL=false");
+        settings.put(Environment.USER, USER);
+        settings.put(Environment.PASS, PASSWORD);
+        settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+        settings.put(Environment.SHOW_SQL, "true");
+        settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+        settings.put(Environment.HBM2DDL_AUTO, "");
 
-    public static SessionFactory getSessionFactory() {
-        if (sessionFactory == null) {
-            try {
-                Configuration configuration = new Configuration();
+        configuration.setProperties(settings);
+        configuration.addAnnotatedClass(User.class);
 
-                Properties settings = new Properties();
-                settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
-                settings.put(Environment.URL, "jdbc:mysql://localhost:3306/sakila?useSSL=false");
-                settings.put(Environment.USER, "root");
-                settings.put(Environment.PASS, "root");
-                settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
-                settings.put(Environment.SHOW_SQL, "true");
-                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
-                settings.put(Environment.HBM2DDL_AUTO, "");
+        ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+            .applySettings(configuration.getProperties())
+            .build();
 
-                configuration.setProperties(settings);
-
-                configuration.addAnnotatedClass(User.class);
-
-                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
-                        .applySettings(configuration.getProperties())
-                        .build();
-
-                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
-
-            } catch (Throwable ex) {
-
-                System.err.println("Initial SessionFactory creation failed." + ex);
-                throw new ExceptionInInitializerError(ex);
-            }
-        }
-        return sessionFactory;
+        sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+      } catch (Throwable ex) {
+        System.err.println("Initial SessionFactory creation failed." + ex);
+        throw new ExceptionInInitializerError(ex);
+      }
     }
-
-
+    return sessionFactory;
+  }
 }
