13eda0738c8d12d0b10eeddee32c93601a397f60
GordinVit <gordin.vit@gmail.com>
2024-07-30 23:29:32 +0300

    codestyle

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,40 +1,40 @@
 package jm.task.core.jdbc.service;
 
-import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
 
-  private final UserDaoHibernateImpl userDaoHibernate = new UserDaoHibernateImpl();
+  private final UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
 
   @Override
   public void createUsersTable() {
-    userDaoHibernate.createUsersTable();
+    userDaoJDBC.createUsersTable();
   }
 
   @Override
   public void dropUsersTable() {
-    userDaoHibernate.dropUsersTable();
+    userDaoJDBC.dropUsersTable();
   }
 
   @Override
   public void saveUser(String name, String lastName, byte age) {
-    userDaoHibernate.saveUser(name, lastName, age);
+    userDaoJDBC.saveUser(name, lastName, age);
   }
 
   @Override
   public void removeUserById(long id) {
-    userDaoHibernate.removeUserById(id);
+    userDaoJDBC.removeUserById(id);
   }
 
   @Override
   public List<User> getAllUsers() {
-    return userDaoHibernate.getAllUsers();
+    return userDaoJDBC.getAllUsers();
   }
 
   @Override
   public void cleanUsersTable() {
-    userDaoHibernate.cleanUsersTable();
+    userDaoJDBC.cleanUsersTable();
   }
 }
